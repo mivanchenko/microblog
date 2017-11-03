2606dd91b236d68b6b071bf40cf34bdc0da45747
Michael Ivanchenko <ivanchenko.michael.work@gmail.com>
2017-11-03 10:54:48 +0200

    Add mysql req; remove db_repository, db; update config

--- config.py
+++ config.py
@@ -1,11 +1,21 @@
 import os
 
 basedir = os.path.abspath(os.path.dirname(__file__))
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
-SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
+#SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repo')
 SQLALCHEMY_TRACK_MODIFICATIONS = True
 
 WTF_CSRF_ENABLED = True
 SECRET_KEY = 'you-will-never-guess'
 
 OPENID_PROVIDERS = [ {'name': 'Yahoo', 'url': 'https://me.yahoo.com'} ]
+
+
+DEBUG = False
+USERNAME = 'dirty_admin'
+PASSWORD = 'da2017'
+
+SQLALCHEMY_DATABASE_URI = 'mysql://' + USERNAME + ':' + PASSWORD \
+    + '@localhost/dirty_blog'
+#SQLALCHEMY_ECHO = True
+
