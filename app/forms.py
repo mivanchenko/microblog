12cb1c102cd171e9963a73fd0b868a6347209271
Michael Ivanchenko <ivanchenko.michael.work@gmail.com>
2018-04-04 11:08:04 +0300

    Add login form

--- app/forms.py
+++ app/forms.py
@@ -1,7 +1,9 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, BooleanField
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
 from wtforms.validators import DataRequired
 
 class LoginForm(FlaskForm):
-    openid = StringField('openid', validators=[DataRequired()])
-    remember_me = BooleanField('remember_me', default=False)
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember_me = BooleanField('Remember Me')
+    submit = SubmitField('Sign In')
--- config.py
+++ config.py
@@ -1,21 +1,24 @@
 import os
 
-basedir = os.path.abspath(os.path.dirname(__file__))
-SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repo')
-SQLALCHEMY_TRACK_MODIFICATIONS = True
+class Config(object):
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
 
-WTF_CSRF_ENABLED = True
-SECRET_KEY = 'you-will-never-guess'
-
-OPENID_PROVIDERS = [ {'name': 'Yahoo', 'url': 'https://me.yahoo.com'} ]
-
-
-DEBUG = False
-USERNAME = 'dirty_admin'
-PASSWORD = 'da2017'
-
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
-#SQLALCHEMY_DATABASE_URI = 'mysql://' + USERNAME + ':' + PASSWORD \
-#    + '@localhost/dirty_blog'
-#SQLALCHEMY_ECHO = True
-
+#basedir = os.path.abspath(os.path.dirname(__file__))
+#SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repo')
+#SQLALCHEMY_TRACK_MODIFICATIONS = True
+#
+#WTF_CSRF_ENABLED = True
+#SECRET_KEY = 'you-will-never-guess'
+#
+#OPENID_PROVIDERS = [ {'name': 'Yahoo', 'url': 'https://me.yahoo.com'} ]
+#
+#
+#DEBUG = False
+#USERNAME = 'dirty_admin'
+#PASSWORD = 'da2017'
+#
+#SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+##SQLALCHEMY_DATABASE_URI = 'mysql://' + USERNAME + ':' + PASSWORD \
+##    + '@localhost/dirty_blog'
+##SQLALCHEMY_ECHO = True
+#
