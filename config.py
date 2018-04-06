561973e1234ab403a07c1108ef2b9705f90e52a5
Michael Ivanchenko <ivanchenko.michael.work@gmail.com>
2018-04-06 11:43:32 +0300

    Add migrations repo

--- config.py
+++ config.py
@@ -1,24 +1,23 @@
 import os
 
+basedir = os.path.abspath(os.path.dirname(__file__))
+
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-#basedir = os.path.abspath(os.path.dirname(__file__))
 #SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repo')
-#SQLALCHEMY_TRACK_MODIFICATIONS = True
 #
 #WTF_CSRF_ENABLED = True
-#SECRET_KEY = 'you-will-never-guess'
 #
 #OPENID_PROVIDERS = [ {'name': 'Yahoo', 'url': 'https://me.yahoo.com'} ]
-#
 #
 #DEBUG = False
 #USERNAME = 'dirty_admin'
 #PASSWORD = 'da2017'
 #
-#SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
 ##SQLALCHEMY_DATABASE_URI = 'mysql://' + USERNAME + ':' + PASSWORD \
 ##    + '@localhost/dirty_blog'
 ##SQLALCHEMY_ECHO = True
-#
