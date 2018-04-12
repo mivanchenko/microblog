f172c2b21e245ec8017dd5ade917ff3ed3daf56d
Michael Ivanchenko <ivanchenko.michael.work@gmail.com>
2018-04-12 18:59:01 +0300

    Start adding following functionality

--- app/models.py
+++ app/models.py
@@ -18,6 +18,20 @@ class User(UserMixin, db.Model):
     about_me = db.Column(db.String(140))
     last_seen = db.Column(db.DateTime, default=datetime.utcnow)
 
+    followers = db.Table( 'followers',
+        db.Column('follower_id', db.Integer, db.ForeignKey('user.id')),
+        db.Column('followed_id', db.Integer, db.ForeignKey('user.id')),
+    )
+
+    followed = db.relationship(
+        'User',
+        secondary=followers,
+        primaryjoin=(followers.c.follower_id == id),
+        secondaryjoin=(followers.c.followed_id == id),
+        backref=db.backref('followers', lazy='dynamic'),
+        lazy='dynamic'
+    )
+
     def __repr__(self):
         return f'<User {self.username}:{self.email}>'
 
@@ -31,6 +45,19 @@ class User(UserMixin, db.Model):
         digest = md5(self.email.lower().encode('utf-8')).hexdigest()
         return f'https://www.gravatar.com/avatar/{digest}?d=identicon&s={size}'
 
+    def follow(self, user):
+        if not self.is_following(user):
+            self.followed.append(user)
+
+    def unfollow(self, user):
+        if self.is_following(user):
+            self.followed.remove(user)
+
+    def is_following(self, user):
+        return self.followed.filter(
+            followers.c.followed_id == user.id
+        ).count() > 0
+
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     body = db.Column(db.String(140))
@@ -39,3 +66,4 @@ class Post(db.Model):
 
     def __repr__(self):
         return f'<Post {self.body}>'
+
